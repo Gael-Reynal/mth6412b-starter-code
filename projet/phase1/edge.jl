058296a7c5eb7b4632271dca3312db426ddce4ca
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-09-20 09:47:22 +0200

    Reprise du code node.jl et adaptation aux arêtes.

---
+++ projet/phase1/edge.jl
@@ -0,0 +1,30 @@
+import Base.show
+
+"""Type abstrait dont d'autres types d'arêtes dériveront."""
+abstract type AbstractEdge{E} end
+
+"""Type représentant les arêtes d'un graphe.
+
+Exemple:
+
+        arête = Edge((Node1, Node2), 12)
+
+"""
+mutable struct Edge{T} <: AbstractEdge{T}
+  parents::T                                #Destiné à être un tuple de noeuds
+  weight::Int64                                
+end
+
+# on présume que toutes les arêtes dérivant d'AbstractEdge
+# posséderont des champs `name` et `parents`.
+
+"""Renvoie le nom du noeud."""
+parents(edge::AbstractEdge) = edge.parents
+
+"""Renvoie les données contenues dans le noeud."""
+weight(edge::AbstractEdge) = edge.weight
+
+"""Affiche un noeud."""
+function show(edge::AbstractEdge)
+  println("Parents: ", parents(edge),", weight: ", weight(edge))
+end
