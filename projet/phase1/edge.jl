e279b7dfb858ad2317471f8093378d539936d9d7
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-09-21 19:22:19 +0200

    limits : tuple de 2 noeuds. limits() renvoie noms

--- projet/phase1/edge.jl
+++ projet/phase1/edge.jl
@@ -11,20 +11,20 @@ Exemple:
 
 """
 mutable struct Edge{T} <: AbstractEdge{T}
-  limits::Vector{Node{T}}                                
+  limits::Tuple{Node{T},Node{T}}                                
   weight::Int64                                
 end
 
 # on présume que toutes les arêtes dérivant d'AbstractEdge
-# posséderont des champs `name` et `parents`.
+# posséderont des champs `weight` et `limits`.
 
-"""Renvoie le nom du noeud."""
-limits(edge::AbstractEdge) = edge.limits
+"""Renvoie le nom des noeuds aux extrémités de l'arête."""
+limits(edge::AbstractEdge)=(edge.limits[1].name,edge.limits[2].name)
 
-"""Renvoie les données contenues dans le noeud."""
+"""Renvoie le poids de l'arête."""
 weight(edge::AbstractEdge) = edge.weight
 
-"""Affiche un noeud."""
+"""Affiche une arête."""
 function show(edge::AbstractEdge)
   println("Noeuds liés: ", limits(edge),", weight: ", weight(edge))
 end
