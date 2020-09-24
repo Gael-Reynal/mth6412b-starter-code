693cefb98b2273703159fe4601fd7810d8bb60ff
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-09-24 16:24:56 +0200

    Liste des noeuds créée

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -19,15 +19,28 @@ add_edge!(G,AC)
 add_edge!(G,BC)
 #show(G)
 
-N,E=read_stsp("bayg29.tsp")
-println(N,E)
 
 function create_graph(filename::String)
     """crée un graphe à partir d'une instance de TSP symétrique dont les poids des arêtes sont donnés au format EXPLICIT.
     Les noms des noeuds seront des entiers de 1 à n, n étant la dimension du graphes.
     Les données qu'ils contiendront seront un tableau de taille 2 de type Array{Float} comportant leurs coordonnées."""
 
-    #On commence par l'identification du header
-    H=read_header(filename)
+    #On commence par récupérer les données du fichier
+    N,E=read_stsp(filename)
 
+    #On crée la liste des noeuds
+    nodes=Node{Array{Float64,1}}[]
+    for node in keys(N)
+        n=Node(string(node),N[node])
+        push!(nodes,n)
+    end
+
+    #On crée la liste des arêtes
+    edges=Edge{}
+    return nodes
+end
+
+graph_nodes=create_graph("bays29.tsp")
+for n in graph_nodes
+    show(n)
 end
