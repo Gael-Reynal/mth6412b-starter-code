a2e296678bac7282ed4415291c7f75173feaece2
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-09-28 16:49:43 +0200

    ajout de using test et fusion avec test.jl

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -3,7 +3,9 @@ include("edge.jl")
 include("graph.jl")
 include("read_stsp.jl")
 
-#if false
+using Test
+
+if false
     A=Node("A","a")
     B=Node("B","b")
     show(A)
@@ -23,7 +25,7 @@ include("read_stsp.jl")
     println(T)
     N=read_nodes(T,"bays29.tsp")
     E=read_edges(T,"bays29.tsp")
-#end
+end
 
 """Renvoie le noeud du graphe dont le nom correspond à celui donné en paramètre"""
 function find_node_of_name(graph::Graph,name::String)
@@ -72,4 +74,25 @@ function create_graph(name::String,filename::String)
     return graph
 end
 
-g=create_graph("g","bays29.tsp")
+fic1 = "../../instances/stsp/brazil58.tsp" 
+fic2 = "../../instances/stsp/swiss42.tsp"
+fic3 = "../../instances/stsp/gr21.tsp"
+
+graph_braz = create_graph("g", fic1)
+graph_swiss = create_graph("g", fic2)
+graph_groet = create_graph("g", fic3)
+
+println(@test typeof(graph_braz) == Graph{Array{Float64, 1}})
+println(@test nb_nodes(graph_braz) == 58)
+println(@test nb_edges(graph_braz) == 58*57/2)
+
+println(@test typeof(graph_swiss) == Graph{Array{Float64, 1}})
+println(@test nb_nodes(graph_swiss) == 42)
+println(@test nb_edges(graph_swiss) == 42*42)
+
+println(@test typeof(graph_groet) == Graph{Array{Float64, 1}})
+println(@test nb_nodes(graph_groet) == 21)
+println(@test nb_edges(graph_groet) == 22*21/2)
+
+g=create_graph("g","../../instances/stsp/bayg29.tsp")
+show(g)
