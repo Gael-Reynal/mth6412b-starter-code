bf9c52d0fafac9777db11bc292e521bdf6ecf0ca
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-10-01 19:38:30 +0200

    ajout des where T, correction erreur l18 (1 -> 2)

--- projet/phase2/kruskal.jl
+++ projet/phase2/kruskal.jl
@@ -1,5 +1,5 @@
 """Renvoie la racine du noeud placé en paramètre"""
-function find_root(n::Node{T})
+function find_root(n::Node{T}) where T
     if n.par==n
         return n
     else
@@ -9,13 +9,13 @@ end
 
 """Applique l'algorithme Kruskal pour renvoyer un arbre 
 de recouvrement minimal du graphe en paramètre"""
-function kruskal(graph::Graph{T})
+function kruskal(graph::Graph{T}) where T
     res=Graph(string("min_tree_",graph.name),graph.nodes,Edge{T}[])
     sort!(graph.edges, by=weight)
 
     for edge in graph.edges
         r1=find_root(edge.limits[1])
-        r2=find_root(edge.limits[1])
+        r2=find_root(edge.limits[2])
         if r1!=r2
             r2.par=r1
             push!(res.edges,edge)
