7024a38a3c0a9304467d3c71c071ae705cd85b5e
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-10-27 10:35:46 +0100

    nom inadapté

--- projet/phase3/prim.jl
+++
@@ -1,8 +0,0 @@
-"""Renvoie la racine du noeud placé en paramètre. Réalise également l'union par le rang"""
-function find_root_rank(n::Node{T}) where T
-    if n.par==n
-        return n
-    else
-        return find_root(n.par)
-    end
-end
