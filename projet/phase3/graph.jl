ffae88393c0a24c62a2bc5945a02a5d25214deca
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-10-31 18:22:40 +0100

    modification du constructeur

--- projet/phase3/graph.jl
+++ projet/phase3/graph.jl
@@ -22,7 +22,9 @@ mutable struct Graph{T} <: AbstractGraph{T}
   edges::Vector{Edge{T}}
 end
 
-Graph{T}(name::String) where T = Graph(name,Node{T}[],Edge{T}[])
+Graph{T}(name::String;
+  nodes::Vector{Node{T}}=Node{T}[],
+  edges::Vector{Edge{T}}=Edge{T}[]) where T = Graph(name,nodes,edges)
 
 """Ajoute un noeud au graphe."""
 function add_node!(graph::Graph{T}, node::Node{T}) where T
