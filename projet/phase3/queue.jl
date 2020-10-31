8c2c009a04cc04d059708c600c278687f9c48d4d
Gael Reynal <gael-simon.reynal@polymtl.ca>
2020-10-31 18:23:40 +0100

    files de priorité

--- projet/phase3/queue.jl
+++ projet/phase3/queue.jl
@@ -1,69 +1,44 @@
-import Base.length, Base.push!, Base.popfirst!
+import Base.length
+import Base.push!
 import Base.show
-import Base.maximum
+import Base.minimum
 
 """Type abstrait dont d'autres types de files dériveront."""
 abstract type AbstractQueue{T} end
 
-"""Type représentant une file avec des éléments de type T."""
-mutable struct Queue{T} <: AbstractQueue{T}
-    items::Vector{T}
-end
-
-Queue{T}() where T = Queue(T[])
-
-"""Ajoute `item` à la fin de la file `s`."""
-function push!(q::AbstractQueue{T}, item::T) where T
-    push!(q.items, item)
-    q
-end
-
-"""Retire et renvoie l'objet du début de la file."""
-popfirst!(q::AbstractQueue) = popfirst!(q.items)
-
-"""Indique si la file est vide."""
-is_empty(q::AbstractQueue) = length(q.items) == 0
-
-"""Donne le nombre d'éléments sur la file."""
-length(q::AbstractQueue) = length(q.items)
-
-"""Affiche une file."""
-show(q::AbstractQueue) = show(q.items)
-
 """File de priorité."""
-mutable struct PriorityQueue{T <: AbstractPriorityItem} <: AbstractQueue{T}
+mutable struct PriorityQueue{T} <: AbstractQueue{T}
     items::Vector{T}
 end
 
 PriorityQueue{T}() where T = PriorityQueue(T[])
 
-maximum(q::AbstractQueue) = maximum(q.items)
+### Getters ###
+length(q::AbstractQueue) = length(q.items)
 
-"""renvoie l'élément de plus haute priorité et le retire de la file"""
-function popfirst_max!(q::AbstractQueue)
-    highest=maximum(q)
-    idx = findall(x -> x == highest, q.items)[1]
-    highest=q.items[idx]
+### Méthodes ###
+
+"""Ajoute un élément à la file"""
+function push!(q::PriorityQueue{T}, item::T) where T
+    push!(q.items,item)
+end
+
+minimum(q::AbstractQueue) = minimum(q.items)
+
+function popfirst_min!(q::AbstractQueue)
+    lowest=minimum(q)
+    idx = findall(x -> x == lowest, q.items)[1]
+    lowest=q.items[idx]
     deleteat!(q.items, idx)
-    highest
+    lowest
 end
 
-"""Insère l'objet `item` à la bonne place dans une file de priorité triée"""
-function push_sort!(q::PriorityQueue,item)
-    push!(q,item)
-    n = length(q.items)
-    while n>1 && q.items[n-1].priority > item.priority
-        q.items[n]=q.items[n-1]
-        q.items[n-1]=item
-        n-=1
-    end
-    q
-end
+is_empty(q::AbstractQueue) = (length(q)==0)
 
 """Affiche une file de priorité"""
 function show(q::PriorityQueue)
     println("La file comporte ",length(q.items)," éléments.")
     for k in q.items
-        println("L'objet ",k.data," est dans la file avec une priorité de ",k.priority)
+        show(k)
     end
 end
